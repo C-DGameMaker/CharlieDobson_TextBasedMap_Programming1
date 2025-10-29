8b1efe82fe91c74669bdf387b9fa9e324b4c638a
C-DGameMaker <charlotte5634@outlook.com>
2025-10-29 15:03:36 -0300

    Added Temp Map and made border work

--- CharlieDobson_TextBasedMap_Programming1/Program.cs
+++ CharlieDobson_TextBasedMap_Programming1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,57 @@ namespace CharlieDobson_TextBasedMap_Programming1
 {
     internal class Program
     {
+        static string[,] map = 
+        {   
+            {"^", "^", "-", "-", "-", "-", "-", "-", "^", "-", "-", "-"},
+            {"-", "^", "-", "-", "-", "-", "~", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "~", "~", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "~", "~", "~", "-", "-", "-", "^"},
+            {"^", "^", "-", "-", "-", "-", "~", "~", "~", "-", "-", "-"},
+            {"~", "~", "~", "-", "-", "-", "-", "-", "~", "-", "-", "-"},
+            {"~", "~", "~", "~", "-", "-", "-", "-", "-", "-", "-", "-"},
+        };
         static void Main(string[] args)
         {
+            DisplayMap();
+            Console.ReadKey(true);
+            DisplayMap(2);
+        }
+
+        static void DisplayMap()
+        {
+            for(int b = 0; b < map.GetLength(1) + 2; b++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("\n");
+            
+            for(int y = 0; y < map.GetLength(0); y++)
+            {
+                Console.Write("║");
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    Console.Write(map[y,x]);
+                }
+                Console.Write("║");
+                Console.Write("\n");
+            }
+
+            for (int b = 0; b < map.GetLength(1) + 2; b++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("\n");
+
+        }
+
+        static void DisplayMap(int scale)
+        {
+            for (int b = 0; b < map.GetLength(1) * scale *+ 2; b++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("\n");
         }
     }
 }
